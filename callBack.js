345e837292810810f16e97cc1f8dea2e9522ea77
almamun528 <ayapths@gmail.com>
2024-10-12 18:55:45 +0600

    callBack

--- callBack.js
+++ callBack.js
@@ -13,3 +13,33 @@ let goodMorning = function(name){
 
 ManagerSaid("Mamun" , sayHello )
 ManagerSaid("Joynal" , goodMorning )
+
+
+//! Calculation
+
+// function calculate (number1,number2, callback){
+//     callback(number1,number2)
+// }
+
+// let addition = function(num,num2){
+//     result = num+num2
+//     console.log("My Addition Result is ", result)
+//     return result
+// }
+
+// calculate(20, 10 , addition )
+
+
+//! odd or even checker 
+
+function oddOrEvenNumber (number, callback){
+    return callback(number)
+}
+
+let inputNumber = function(digite){
+    console.log(digite + " is ",digite % 2 == 0 ? "Even Number" : "Odd Number")
+
+}
+
+oddOrEvenNumber(23 ,inputNumber )
+
